b077b866bfbed30970b9f2d2f86eb30b7449df60
vijayvishwakarma <jeet.lsi2011@gmail.com>
2018-05-31 13:49:01 -0400

    database changed for facebook

--- scripts/create_database.js
+++ scripts/create_database.js
@@ -12,10 +12,14 @@ var connection = mysql.createConnection(dbconfig.connection);
 connection.query('\
 CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.users_table + '` ( \
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
+    `facebook_id` VARCHAR(20), \
     `username` VARCHAR(20) NOT NULL, \
     `password` CHAR(60) NOT NULL, \
+    `facebook_token` VARCHAR(20), \
         PRIMARY KEY (`id`), \
     UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
+    UNIQUE INDEX `facebook_id_UNIQUE` (`facebook_id` ASC), \
+    UNIQUE INDEX `facebook_token_UNIQUE` (`facebook_token` ASC), \
     UNIQUE INDEX `username_UNIQUE` (`username` ASC) \
 )');
 
